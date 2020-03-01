348fd202fc6a9cd717d163aadc4b14edcc1eb8ca
Ata <ca.eyuboglu@gmail.com>
2020-03-01 15:45:13 +0300

    Initial Commit

---
+++ src/pages/twitter/Twitter.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Twitter extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+               Twitter Sayfası 
+            </div>
+        )
+    }
+}
+
+export default Twitter
---
+++ src/pages/whatsapp/Whatsapp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Whatsapp extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Whatsapp Sayfası.
+            </div>
+        )
+    }
+}
+
+export default Whatsapp
